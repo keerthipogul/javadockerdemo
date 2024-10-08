8dd569bfc142f6dfb56cfaeaf1e9a8b94fbf8db3
pichu <pichukakeerthi@gmail.com>
2024-10-08 14:57:59 +0530

    world from

--- src/main/java/com/neoteric/learning/App.java
+++ src/main/java/com/neoteric/learning/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "hi from World!" );
+        System.out.println( "hi fromm too  World!" );
     }
 }
